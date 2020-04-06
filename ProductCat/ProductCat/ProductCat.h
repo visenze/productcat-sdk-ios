8e78ccaaefdb078d365bd477e129e1d06da1b001
Hung <hung@visenze.com>
2020-04-06 23:13:45 +0800

    initial setup

---
+++ ProductCat/ProductCat/ProductCat.h
@@ -0,0 +1,19 @@
+//
+//  ProductCat.h
+//  ProductCat
+//
+//  Created by Hung on 6/4/20.
+//  Copyright © 2020 ViSenze. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProductCat.
+FOUNDATION_EXPORT double ProductCatVersionNumber;
+
+//! Project version string for ProductCat.
+FOUNDATION_EXPORT const unsigned char ProductCatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProductCat/PublicHeader.h>
+
+
