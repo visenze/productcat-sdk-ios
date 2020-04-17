a1e948fec06982277719d089cf528c431b0f368c
Hung <hung@visenze.com>
2020-04-17 14:23:56 +0800

    add dependencies

---
+++ ProductCatExample/Pods/Target Support Files/Pods-ProductCatExample-ProductCatExampleUITests/Pods-ProductCatExample-ProductCatExampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProductCatExample_ProductCatExampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ProductCatExample_ProductCatExampleUITests
+@end
---
+++ ProductCatExample/Pods/Target Support Files/Pods-ProductCatExampleTests/Pods-ProductCatExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProductCatExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_ProductCatExampleTests
+@end
